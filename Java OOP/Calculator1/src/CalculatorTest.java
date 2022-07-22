d8d65fc3acb0b5be931df4a9b93e7ed5e2a0bd51
Reem310 <106007879+Reem310@users.noreply.github.com>
2022-07-22 17:16:04 +0300

    Update CalculatorTest.java

--- Java OOP/Calculator1/src/CalculatorTest.java
+++ Java OOP/Calculator1/src/CalculatorTest.java
@@ -4,6 +4,7 @@ public class CalculatorTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calculator operation1 = new Calculator();
+		System.out.println("This is a simple calculator that only add or subtract two numbers.");
 		operation1.calculator();
 		System.out.println("The result is: " + operation1.getResults());
 	}
